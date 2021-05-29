1182afa3df51401d49dde4496c60614918615349
netwong <netwong@gmail.com>
2021-05-29 13:34:10 +0800

    push

--- pygame-1.py
+++ pygame-1.py
@@ -17,11 +17,24 @@ while run:
     pygame.time.delay(100)
 
     for event in pygame.event.get():
-        if event.type = pygame.QUIT:
+        if event.type == pygame.QUIT:
             run = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+
+    if keys[pygame.K_RIGHT]:   
+        x += vel
+
+    if keys[pygame.K_UP]:
+        y -= vel 
+
+    if keys[pygame.K_DOWN]:
+        y += vel
 
     pygame.draw.rect(win, (255,0,0), (x,y,width,height))
-    pygram.display.update()
+    pygame.display.update()
 
 pygame.quit()
