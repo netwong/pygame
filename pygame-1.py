2d446a92ee16214d341dda9887b5d4fa92662628
netwong <netwong@gmail.com>
2021-05-31 13:10:45 +0800

    main

--- pygame-1.py
+++ pygame-1.py
@@ -2,15 +2,15 @@ import pygame
 pygame.init()
 
 win = pygame.display.set_mode((500,500))
-
 pygame.display.set_caption("First Game")
 
+
+screenwidth = 500
 x = 50
 y = 50
 width = 40
 height = 60
 vel = 5
-
 
 run = True
 while run:
@@ -22,18 +22,16 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > 0: 
         x -= vel
-
-    if keys[pygame.K_RIGHT]:   
+    if keys[pygame.K_RIGHT] and ((x + width) < screenwidth):   
         x += vel
-
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and y > 0:
         y -= vel 
-
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and ((y + height) < screenwidth):
         y += vel
 
+    win.fill((0,0,0))
     pygame.draw.rect(win, (255,0,0), (x,y,width,height))
     pygame.display.update()
 
