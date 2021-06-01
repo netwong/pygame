04a3365afefe9cd7f99f5ea5713ce6bb37486c84
= <netwong@gmail.com>
2021-06-01 10:13:26 +0800

    main

--- pygame-1.py
+++ pygame-1.py
@@ -5,12 +5,23 @@ win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("First Game")
 
 
-screenwidth = 500
+
+walkRight =[pygame.image.load('R1.png'),pygame.image.load('R2.png'),pygame.image.load('R3.png'),pygame.image.load('R4.png'),pygame.image.load('R5.png'),pygame.image.load('R6.png'),pygame.image.load('R7.png'),pygame.image.load('R8.png'),pygame.image.load('R9.png')]
+walkLeft =[pygame.image.load('L1.png'),pygame.image.load('L2.png'),pygame.image.load('L3.png'),pygame.image.load('L4.png'),pygame.image.load('L5.png'),pygame.image.load('L6.png'),pygame.image.load('L7.png'),pygame.image.load('L8.png'),pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
 x = 50
-y = 50
-width = 40
-height = 60
+y = 425
+width = 64
+height = 64
 vel = 5
+isJump  = False
+jumpCount = 10
+left = False
+right = False
+walkCount = 0
+
 
 run = True
 while run:
@@ -22,15 +33,24 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > 0: 
+    if keys[pygame.K_LEFT] and x > vel: 
         x -= vel
-    if keys[pygame.K_RIGHT] and ((x + width) < screenwidth):   
+    if keys[pygame.K_RIGHT] and ( x < 500 - width - vel):   
         x += vel
-    if keys[pygame.K_UP] and y > 0:
-        y -= vel 
-    if keys[pygame.K_DOWN] and ((y + height) < screenwidth):
-        y += vel
-
+    if not (isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+            
     win.fill((0,0,0))
     pygame.draw.rect(win, (255,0,0), (x,y,width,height))
     pygame.display.update()
